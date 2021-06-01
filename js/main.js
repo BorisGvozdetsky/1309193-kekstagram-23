41f205ef71eef6fc53177a1ae5e75c7e809cd080
BorisGvozdetsky <borisgvozdetsky@gmail.com>
2021-06-01 16:10:17 +0300

    Рефакторит код служебных функций

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
+const IS_LONG = 140;
+
 const getRandomNumber = function (min, max) {
   if (min < 0 || max < 0) {
-    return console.log('Вы ввели отрицательное число!');
+    return false;
   }
   return Math.round(min + Math.random() * (max - min));
 };
 
-getRandomNumber(-1, 77);
-
-const IS_LONG = 140;
 const checkStringLength = function (string) {
   return string.length < IS_LONG ? string : 'Строка слишком длинная';
 };
 
-console.log(checkStringLength('I love JavaScript'));
+getRandomNumber(2, 77);
+checkStringLength('I love JavaScript');
